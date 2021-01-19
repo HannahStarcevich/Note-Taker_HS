360bf7e82b4d2e11674fb893937cedb307945b09
HannahStarcevich <starcevichh@gmail.com>
2021-01-19 15:09:15 -0800

    updates to routes

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // connect to express
+const {
+    response
+} = require("express");
 const express = require("express");
+
+const fs = require("fs")
 
 // set up for connecting through file paths
 const path = require("path");
@@ -7,23 +12,86 @@ const path = require("path");
 //call the express function
 const app = express();
 
+app.use(express.static("public"))
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+
+
 // identify the port connection 
 const PORT = process.env.PORT || 3000;
 
-// identify the request address and send a response
-app.get("/", (req, res) => {
-    res.send("this is it!")
-})
-
 // set up URL for html page
-app.get("/home", (req, res) => {
-    res.sendFile(path.join(_dirname, "./public/index.html"))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
-// connect CSS stylesheet
-app.use(express.static("./public/assets/css/style.css"))
+// set up URL for notes page
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+})
 
+// let notes = []
+
+// Grab the full note database from db.json
+app.get("/api/notes", (req, res) => {
+
+    // res.sendFile(path.join(__dirname, "db/db.json"))
+    // console.log(notes)
+    // console.log(response)
+
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
+        if (err) throw error
+        console.log("this is data", data)
+        // notes = JSON.parse(data)
+        res.json(JSON.parse(data))
+    })
+
+})
+
+// Create New Note - takes in JSON input
+app.post("/api/notes", function (req, res) {
+
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
+        if (err) throw error
+        let notes = JSON.parse(data)
+
+        var newNote = req.body;
+        notes.push(newNote);
+        var idNumber = 0
+        for (var i = 0; i < notes.length; i++) {
+            notes[i].id = idNumber
+            idNumber++
+        }
+        console.log(notes)
+
+        fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
+            if (err) return console.log(err);
+            res.json(true)
+        })
+    })
+});
+
+// // Displays a single note, or shows "No note found"
+// app.get("/api/notes/:id", function (req, res) {
+//     var noteSelected = req.params.notes;
+
+//     console.log(chosen);
+
+//     for (var i = 0; i < notes.length; i++) {
+//         if (chosen === notes[i].routeName) {
+//             return res.json(notes[i]);
+//         }
+//     }
+
+//     return res.send("No note found");
+
+// });
 
 
 
